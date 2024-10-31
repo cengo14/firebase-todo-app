5a5b51aff1fa845571827a78ad9af103d35e26a7
cengo14 <cengizaydemir@live.com>
2024-11-01 00:08:20 +0300

    hata giderme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Register from "./pages/register";
+
 import { Toaster } from "react-hot-toast";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Modal from "./component/Modal";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Toaster position="top-right" />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
